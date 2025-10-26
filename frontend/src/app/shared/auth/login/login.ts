b9bf730d9244f6fc61880fc66fa8ddac71250b88
nabil bennacer <nabil.bennacer@etu.umontpellier.fr>
2025-10-26 22:54:50 +0100

    ajout du composat login

--- frontend/src/app/shared/auth/login/login.ts
+++ frontend/src/app/shared/auth/login/login.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthServices } from '../auth-services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports : [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
-export class Login {
+export class LoginComponent {
+  readonly auth = inject(AuthServices);
+  readonly router = inject(Router);
+  readonly form = new FormGroup({
+    login: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required] })
+  });
 
+  constructor() {
+    effect(() => {
+      if (this.auth.isLoggedIn()) {
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+
+  onSubmit() {
+    const login = this.form.value.login;
+    const password = this.form.value.password;
+    if (login && password) {
+      this.auth.login(login, password);
+    }
+  }
 }
