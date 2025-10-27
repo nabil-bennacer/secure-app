bfa3f4e67a764f8cd28ec570e79fc58a858907a2
nabil bennacer <nabil.bennacer@etu.umontpellier.fr>
2025-10-27 20:46:32 +0100

    debut dockerisation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -39,7 +39,7 @@ app.use(cookieParser())
 
 // Configuration CORS : autoriser le front Angular en HTTPS local
 app.use(cors({
-  origin: 'https://localhost:4200',
+  origin: 'https://localhost:8080',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
