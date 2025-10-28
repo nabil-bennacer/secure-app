631b9c326d0016c9bf071d006c526dbdb4c8a00d
nabil bennacer <nabil.bennacer@etu.umontpellier.fr>
2025-10-28 02:27:41 +0100

    dockerisation avec front en https

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,7 +41,7 @@ app.use(cookieParser())
 
 // Configuration CORS : autoriser le front Angular en HTTPS local
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: 'https://localhost:8080',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
